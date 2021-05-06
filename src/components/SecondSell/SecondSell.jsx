b98b9e344ae6a41b4cd29dd61fb724d884618bc2
Daynamarie <Dayna.christison@gmail.com>
2021-05-06 15:44:59 -0400

    second sell first round styling

--- src/components/SecondSell/SecondSell.jsx
+++ src/components/SecondSell/SecondSell.jsx
@@ -7,12 +7,12 @@ import './SecondSell.css';
 const SecondSell = () => {
   return (
     <div className='second-sell'>
-     <div className="product">
+     <div className="selling-point">
         <img className="second-product" src="https://wizerlist.com/wp-content/uploads/2020/06/oticon-2.jpg" alt="Left and right silver hearing aid pieces" />
-        <div className="detail">
-          <h2 className="name">Why Oticon More Technology?</h2>
-          <p className="price">Patients are constantly presented with complex, unpredictable sound scenes. It’s the role of the brain to help make meaning of it all. Oticon More is designed to work more like the brain, because it learned through experience.ticon More delivers 30% more sound to the brain, giving the brain more of what it needs to make better sense of sound, so you can get more out of life.</p>
-          <h3 className="description">FOR A LIMITED TIME ONLY WHEN YOU SIGN UP</h3>
+        <div className="second-info">
+          <h2 className="question">Why Oticon More Technology?</h2>
+          <p className="answer">Patients are constantly presented with complex, unpredictable sound scenes. It’s the role of the brain to help make meaning of it all. Oticon More is designed to work more like the brain, because it learned through experience.ticon More delivers 30% more sound to the brain, giving the brain more of what it needs to make better sense of sound, so you can get more out of life.</p>
+          <h3 className="limited">FOR A LIMITED TIME ONLY WHEN YOU SIGN UP</h3>
         </div>
       </div>
       <Form />
